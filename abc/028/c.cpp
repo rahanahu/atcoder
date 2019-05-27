9e368f598d4ccf44ea44572e1942e2463fbe09d7
raha <rahanahu@gmail.com>
2019-05-27 23:18:55 +0900

    028c

---
+++ abc/028/c.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include <utility>
+
+using namespace std;
+
+int main() {
+
+	vector<int> N(5, 0);
+	vector<int> ans(10, 0);
+	
+	for (auto i = 0; i < 5; i++) {
+		cin >> N[i];
+	}
+	int a, b;
+	a = N[0] + N[3] + N[4];
+	b = N[1] + N[2] + N[4];
+
+
+	if (a > b) {
+		cout << a << endl;
+	}
+	else {
+		cout << b << endl;
+	}
+
+
+	return 0;
+}
